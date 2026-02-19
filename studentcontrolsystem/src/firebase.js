875e3e8fa95b9c29de15c55f2ccf855f69acb26c
Orhan Yalcindag <orhanyalcindag@gmail.com>
2026-02-19 14:42:38 +0300

    kritik: firebase paketi bagimliliklara eklendi

--- studentcontrolsystem/src/firebase.js
+++ studentcontrolsystem/src/firebase.js
@@ -1,10 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; //
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // Buranın doğruluğundan emin olalım
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
